07a96a8414e0afcd897d9cac582f57bebe488949
David Choi <davidgumzchoi@gmail.com>
2021-08-12 15:28:33 -0700

    davidschoi committed

---
+++ README.md
@@ -0,0 +1,14 @@
+# davidschoi
+
+> Inspired by [kentcdodds](https://kentcdodds.com/)
+
+## Usage
+
+```javascript
+var davidschoi = require('davidschoi');
+console.log(davidschoi.name.first); // 'Kent'
+```
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,67 @@
+module.exports = {
+	name: {
+		first: 'David',
+		middle: 'Seunghyun',
+		last: 'Choi',
+	},
+	birthday: '1988-07-13',
+	email: 'me@davidschoi.com',
+	website: 'https://www.davidschoi.com/',
+	twitter: 'https://twitter.com/thegreatgumz',
+	github: 'https://github.com/greatgumz',
+	linkedin: 'https://www.linkedin.com/in/greatgumz/',
+	stackoverflow: 'https://stackoverflow.com/users/3713756/gumz',
+	facebook: 'https://www.facebook.com/greatgumz',
+	experience: [
+		{
+			employer: 'Carta',
+			title: 'Senior Software Engineer',
+			start: 'Jan 2021',
+			end: 'present',
+		},
+		{
+			employer: 'Self-employed',
+			title: 'Web Development Consultant',
+			start: 'Oct 2016',
+			end: 'Jul 2021',
+		},
+		{
+			employer: 'Twitch',
+			title: 'Front End Engineer II',
+			start: 'Mar 2018',
+			end: 'Jan 2021',
+		},
+		{
+			employer: 'Solver',
+			title: 'Developer',
+			start: 'Jan 2017',
+			end: 'Feb 2018',
+		},
+		{
+			employer: 'Carta',
+			title: 'Senior Software Engineer',
+			start: 'Jan 2021',
+			end: 'present',
+		},
+		{
+			employer: 'CPQ Solutions',
+			title: 'Senior Implementation Consultant',
+			start: 'Sep 2012',
+			end: 'Aug 2016',
+		},
+		{
+			employer: 'Sungevity',
+			title: 'Technical Sales Associate',
+			start: 'Jul 2011',
+			end: 'Sep 2012',
+		},
+	],
+	education: {
+		college: 'University of California, Berkeley',
+		graduated: 'May 2011',
+		degree: 'Bachelor of Arts',
+		major: 'Sociology',
+		gpa: '3.7',
+	},
+	location: 'Orange County, CA',
+};
